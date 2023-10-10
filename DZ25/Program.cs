91d3af5c50b4f2e6bc8e8f05af67a849ffbcfd92
iamsk46 <lomalstekloy@gmail.com>
2023-10-10 09:16:15 +0300

    Pochti osili DZ

---
+++ DZ25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.WriteLine("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int sum = A;
+
+for (int i = 1; i < B ; i++)
+{
+    sum = sum * A;
+}
+System.Console.WriteLine($"{A} в степени {B} равно: " +sum);
---
+++ DZ27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// System.Console.WriteLine("Введите число: ");
+// string msg = System.Console.ReadLine();
+// char[] array = msg.ToCharArray();
+// int sum = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     sum += array[i];
+// }
+
+// System.Console.WriteLine(sum);
+
+
+System.Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(System.Console.ReadLine());
+int sum = 0;
+while (N > 0)
+{
+    int num = N % 10;
+    N = N / 10;
+    sum = sum + num;
+}
+System.Console.WriteLine("Суммой является: " + sum);
+
